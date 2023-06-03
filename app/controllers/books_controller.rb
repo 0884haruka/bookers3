667db5e5a381a83ed052a6fdfa2865c7c4729121
EC2 Default User <ec2-user@ip-172-31-11-222.ap-northeast-1.compute.internal>
2023-06-03 11:34:31 +0000

    再制作6

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,36 @@
 class BooksController < ApplicationController
-  
+
   def index
     @user = current_user
     @users = User.all
     @books = Book.all
     @book = Book.new
   end
-  
+
   def show
-    @book = @Book.find(params[:id])
+    @book = Book.find(params[:id])
     @user = @book.user
   end
-  
+
   def edit
     @book = @Book.find(params[:id])
   end
   
   def update
+    @book = Book.find(params[:id])
+      if @book.update(book_params)
+        flash[:notice] = "You have updated book successfully."
+        redirect_to  book_path(@book.id)
+      else
+        render :edit
+      end
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @user = current_user
-    if @book.save
+    if @book.save!
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
@@ -31,11 +38,11 @@ class BooksController < ApplicationController
       render :index
     end
   end
-  
+
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-     @books = @user.books
-     @book = Book.new
+    @books = @user.books
+    @book = Book.new
   end
   
   def edit
